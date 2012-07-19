869269bb3076c94b37617c425e3e372ae1d0f610
Alexander Teves <alexander.teves@gmail.com>
2012-07-19 15:16:23 +0200

    Removed boolean check, changed sort order to asc

--- JavaScript/bubblesort.js
+++ JavaScript/bubblesort.js
@@ -1,16 +1,14 @@
 function bsort(l) {
-	var swap = false;
-	var tmp;
-	for(x in l) {
-		if(x < (l.length-1)) {
-			if(l[x] < l[parseInt(x)+1]) {
-				swap = true;
-				tmp = l[x];
-				l[x] = l[parseInt(x)+1];
-				l[parseInt(x)+1] = tmp
-			}
-		}
-		if(swap) {bsort(l)}
-	}
-	return l
+    var tmp;
+    for(x in l) {
+        if(x < (l.length-1)) {
+            if(l[x] > l[parseInt(x)+1]) {
+                tmp = l[x];
+                l[x] = l[parseInt(x)+1];
+                l[parseInt(x)+1] = tmp;
+                bsort(l)
+            }
+        }
+    }
+    return l
 }
